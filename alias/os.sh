6f043e62f3bb02901792275828007d0fa1e9d31b
Meiti <32864532+meiti-x@users.noreply.github.com>
2024-02-26 10:55:02 +0330

    Update os.sh

--- alias/os.sh
+++ alias/os.sh
@@ -3,10 +3,3 @@ alias ..='cd ../'
 alias ..2='cd ../../'
 alias ..3='cd ../../../'
 alias ..4='cd ../../../../'
-
-
-# exa aliases
-alias l='exa'                                                          # ls
-alias ll='exa -lbF --git'                                                # list, size, type, git
-alias lf='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias tree='exa --tree --level=2'             
